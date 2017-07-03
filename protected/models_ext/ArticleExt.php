93596dcb6ab1b042774fca10299edec7fc690d1f
ztenst <754710007@qq.com>
2017-07-03 22:31:49 +0800

    youhua

--- protected/models_ext/ArticleExt.php
+++ protected/models_ext/ArticleExt.php
@@ -72,8 +72,8 @@ class ArticleExt extends Article{
             } else
                 $this->created = $this->updated = time();
         }
-        else
-            $this->updated = time();
+        // else
+        //     $this->updated = time();
         return parent::beforeValidate();
     }
 
@@ -90,7 +90,7 @@ class ArticleExt extends Article{
             ),
             'normal' => array(
                 'condition' => "{$alias}.status=1 and {$alias}.deleted=0 and {$alias}.is_top_video=0",
-                'order'=>"{$alias}.sort desc,{$alias}.updated desc",
+                'order'=>"{$alias}.updated desc,{$alias}.sort desc",
             ),
             'undeleted' => array(
                 'condition' => "{$alias}.deleted=0",
@@ -98,7 +98,7 @@ class ArticleExt extends Article{
             ),
             'isvideo'=>array(
                 'condition' => "{$alias}.status=1 and {$alias}.is_top_video=1 and {$alias}.deleted=0",
-                'order'=>"{$alias}.sort desc,{$alias}.updated desc",
+                'order'=>"{$alias}.updated desc,{$alias}.sort desc",
             ),
         );
     }
